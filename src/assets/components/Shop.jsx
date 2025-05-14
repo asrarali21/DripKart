a25653e755c6257f84d1d115e518dc7a07406712
Asrar Ali <mohdasrar3825@gmail.com>
2025-05-14 12:14:27 +0530

    implemented Search feature

--- src/assets/components/Shop.jsx
+++ src/assets/components/Shop.jsx
@@ -8,6 +8,8 @@ function Shop() {
   }
 
    const [Storedata, setStoredata] = useState([])
+   const [productname, setproductname] = useState("")
+  const [FilterProduct, setFilterProduct] = useState([])
    const navigate = useNavigate(); 
 
    const api = "https://fakestoreapi.com/products"
@@ -27,27 +29,56 @@ function Shop() {
       Datafetch(); 
     }, []);
     
-    
-
+    useEffect (()=>{
+    let filteredProducts = Storedata.filter((item , i)=>{
+      if (item.title.toLowerCase().includes(productname.toLowerCase())) {
+        return true ;
+      }
+  })
+  console.log(filteredProducts);
+  setFilterProduct(filteredProducts);
+  
+  }, [productname,Storedata])
+   
   return (
     <>
+
     <div className="shopnav-container"> 
   <NavLink to="/mencollection"  >Mens Collection</NavLink>
   <NavLink to="/Jewellery" >Jewellery</NavLink>
   <NavLink to="/womencollection" >Women Collection</NavLink>
   <NavLink to="/electronic" >Electronic Section</NavLink>
     </div>
+       <input type="text" 
+       className='search-input'
+       value={productname}
+       placeholder='Search products here'
+       onChange={(e)=>setproductname(e.target.value)}
+       />
       <h1 className='All-product-heading'>All Product</h1>
+      { FilterProduct.length === 0 ? <h3 className='product-not-found'>Product Not Found</h3> : null }
     <div className='cart-data'>
-     {Storedata.map((item)=>{
-       return (
-         <div key={item.id} className='cart-container' onClick={()=>handleclick(item.id)}>
-          <img src={item.image} alt="" />
-          <p>{item.title}</p>
-          <p> ${item.price}</p>
-        </div>
-      )
-    })}
+    { FilterProduct.length === 0 && productname === "" ? (
+      Storedata.map((item) => {
+        return (
+          <div key={item.id} className='cart-container' onClick={() => handleclick(item.id)}>
+            <img src={item.image} alt="" />
+            <p>{item.title}</p>
+            <p> ${item.price}</p>
+          </div>
+        );
+      })
+    ) : (
+      FilterProduct.map((item) => {
+        return (
+          <div key={item.id} className='cart-container' onClick={() => handleclick(item.id)}>
+            <img src={item.image} alt="" />
+            <p>{item.title}</p>
+            <p> ${item.price}</p>
+          </div>
+        );
+      })
+    )}
     </div>
     </>
   )
